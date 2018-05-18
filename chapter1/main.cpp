525ef87ec610c839a38783a7509d1ea4ad99d331
Joshua Turner <joshflyer5tp@gmail.com>
2018-05-18 02:35:19 -0400

    basic printing

--- chapter1/main.cpp
+++ chapter1/main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
+#include <iomanip>
 
 int main ()
 {
     std::cout << "Hello, udemy users!\n";
-    std::cout << 4080;
+    std::cout << 4080 << std::endl;
+
+    std::cout << std::setw (5) << 2018 << " is today's year." << std::endl;
+    std::cout << std::setw (5) << 5 << " is today's month." << std::endl;
+    std::cout << std::setw (5) << 18 << " is today's day." << std::endl;
 
     return 0;
 }
